2939facdbfd5015881f07caf9f0bb9227413f566
7henoble1 <61429724+7henoble1@users.noreply.github.com>
2020-05-23 04:26:05 -0700

    Updates

---
+++ precache-manifest.2210a82c47c1bff276e42590ebec44e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6defc184fa566b27f300f57d426d779",
+    "url": "/covid-19_tracker/index.html"
+  },
+  {
+    "revision": "c0c507b8f2fc4176d3d4",
+    "url": "/covid-19_tracker/static/css/main.53f4a085.chunk.css"
+  },
+  {
+    "revision": "0099ed01a7ad87f64271",
+    "url": "/covid-19_tracker/static/js/2.ab67c011.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19_tracker/static/js/2.ab67c011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c507b8f2fc4176d3d4",
+    "url": "/covid-19_tracker/static/js/main.4dbe66c5.chunk.js"
+  },
+  {
+    "revision": "9787552723b54eb756e5",
+    "url": "/covid-19_tracker/static/js/runtime-main.6b969f26.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_tracker/static/media/image.d7265326.png"
+  }
+]);
